012416ecbf4949f51fbe46c0f30af31b7e8db3c3
Scott Hurlow <Scottyhurlow@gmail.com>
2019-06-12 09:53:54 -0700

    final version of source files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
 import React, { Component } from 'react';
-import Search from './Search'
-import PokemonList from './PokemonList'
 
-class App extends Component {
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      pokedex: []
-    }
-  }
-
-  addToPokedex = (pokemon) => {
-    if (pokemon.name) {
-      this.setState({
-        pokedex: [...this.state.pokedex, pokemon]
-      })      
-    }
-  }
-
-  render() {
-    return (
-      <div className="pokedex">
-        <h1 className='nav'>Ghrsea03 Pokedex</h1>
-        <Search addToPokedex={this.addToPokedex}/>
-        <PokemonList pokedex={this.state.pokedex}/>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>Ghrsea03 Pokedex</div>
+  )
 }
-
 
 export default App;

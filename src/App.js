6e67b947589815eb5d51f950601671f270c0b060
Scott Hurlow <Scottyhurlow@gmail.com>
2019-06-12 10:12:28 -0700

    improves solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import Search from './Search'
+import PokemonList from './PokemonList'
 
-const App = () => {
-  return (
-    <div>Ghrsea03 Pokedex</div>
-  )
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pokedex: []
+    }
+  }
+
+  addToPokedex = (pokemon) => {
+    if (pokemon.name) {
+      this.setState({
+        pokedex: [...this.state.pokedex, pokemon]
+      })      
+    }
+  }
+
+  render() {
+    return (
+      <div className="pokedex">
+        <h1 className='nav'>Ghrsea03 Pokedex</h1>
+        <Search addToPokedex={this.addToPokedex}/>
+        <PokemonList pokedex={this.state.pokedex}/>
+      </div>
+    )
+  }
 }
+
 
 export default App;
